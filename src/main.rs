413188b04617b24c2f9474c18d006acf5d108ef1
James Andrews <bdaddyja@gmail.com>
2019-09-19 20:16:33 -0700

    Testing module imports

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod configuration;
+use crate::configuration::config::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let d = configuration::httpdconf::HttpdConf {filepath: "/desktop/files"};
+    println!("{:?}", d.lookup());
 }
