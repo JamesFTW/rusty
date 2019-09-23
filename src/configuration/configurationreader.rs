c788f392fbbb77452a2d3bb8d981dc265da0bca3
James Andrews <bdaddyja@gmail.com>
2019-09-23 02:35:25 -0700

    Configuration reader implemented as factory

---
+++ src/configuration/configurationreader.rs
@@ -0,0 +1,18 @@
+
+use super::httpdconf::HttpdConf as HttpdConf;
+use super::mimetypes::MimeTypeConf as MimeTypeConf;
+use super::config::ConfigType as ConfigType;
+
+#[derive(Debug)]
+pub struct ConfigurationReader;
+
+impl ConfigurationReader {
+  pub fn get_config(str: &str) -> ConfigType {
+    match str.as_ref() {
+      "HTTPD_CONF" => ConfigType::HttpdConf(HttpdConf::new("./conf/httpd.conf")),
+      "MIME_TYPE" => ConfigType::MimeTypeConf(MimeTypeConf::new("./conf/mime.types")),
+      &_ => unimplemented!()
+    }
+  }
+}
+
