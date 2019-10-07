6f53e29e28478a2a1d591f1e903dd37ae2c852a5
James Andrews <bdaddyja@gmail.com>
2019-10-07 00:35:22 -0700

    Better method name

--- src/configuration/configurationreader.rs
+++ src/configuration/configurationreader.rs
@@ -7,7 +7,7 @@ use super::config::ConfigType as ConfigType;
 pub struct ConfigurationReader;
 
 impl ConfigurationReader {
-  pub fn get_config(str: &str) -> ConfigType {
+  pub fn config(str: &str) -> ConfigType {
     match str.as_ref() {
       "HTTPD_CONF" => ConfigType::HttpdConf(HttpdConf::new("./conf/httpd.conf")),
       "MIME_TYPE" => ConfigType::MimeTypeConf(MimeTypeConf::new("./conf/mime.types")),
